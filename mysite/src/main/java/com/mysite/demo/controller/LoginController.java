6655cc74d4b237022be01f1da90a69a5de79f4c3
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-03-20 17:29:04 +0900

    0320:login 뷰 및 login 메서드 만들기3

--- mysite/src/main/java/com/mysite/demo/controller/LoginController.java
+++ mysite/src/main/java/com/mysite/demo/controller/LoginController.java
@@ -3,13 +3,19 @@ package com.mysite.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mysite.demo.dto.LoginRequest;
 import com.mysite.demo.entity.Member;
 import com.mysite.demo.service.MemberService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/cookie")
@@ -37,7 +43,8 @@ public class LoginController {
 		return "login/main";
 	}
 
-
+	
+	//Login(Get)
 	@GetMapping("/login")
 	public String loginPage(Model model) {
 		
@@ -47,5 +54,22 @@ public class LoginController {
 		model.addAttribute("loginRequest", new LoginRequest());
 		
 		return "login/login";
+	}
+
+
+	//Login(Post)
+	@PostMapping("/login")
+	@ResponseBody
+	public String login(
+			@ModelAttribute LoginRequest loginRequest,
+			BindingResult bindingResult,
+			HttpServletResponse response,
+			Model model
+			) {
+		
+		model.addAttribute("loginType", "cookie");
+		model.addAttribute("pageTitle", "Cookie Login");
+		
+		return String.valueOf(loginRequest.getId() + loginRequest.getPw());
 	}
 }
